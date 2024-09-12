00954bc0f4ed12885dd06cf6f7db516ee186ddec
Henrique de Souza <rike1se@gmail.com>
2024-09-12 11:44:30 -0300

    criando repositório e subindo exercicio competição e libertadores

---
+++ competicao/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //declaração das váriaveis 
+        int n = 0, m = 0 , resultado = 0, diagP = 0, diagS = 0;
+        // estrutura de repetição
+        do
+        {
+            //entrada dos dados
+            string linha = Console.ReadLine();
+            string[] valores = linha.Split(' ');
+            n = int.Parse(valores[0]);
+            m = int.Parse(valores[1]);
+
+            //condição de inicio de casos de teste que o exercíco pede
+            if (n >= 3 && m <= 100)
+            {
+
+                //matriz para entrada dos casos
+                int[,] mat = new int[n, m];
+                for (int i = 0; i < mat.GetLength(0); i++)
+                {
+                    linha = Console.ReadLine();
+                    valores = linha.Split(' ');
+                    for (int j = 0; j < mat.GetLength(1); j++)
+                    {
+                    mat[i, j] = int.Parse(valores[j]);
+                    }
+                }
+                
+                //percorrer matriz para verificar
+                for (int i = 0; i < mat.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mat.GetLength(1); j++)
+                    {
+
+                        //somar diagonais para saber se todos os casos foram resolvidos
+                        diagP += mat[i,i];
+                        diagS += mat[i, mat.GetLength(1) -1 -i];
+                        //opção 1 e 2 do caso de teste
+                        resultado += mat[i,j];
+
+                    }
+                }
+                        //saída dos dados
+                        //verificação dos casos
+                        if (resultado == 0)
+                        {
+                            Console.WriteLine(1);
+                        }
+                        else if (resultado != 0)
+                        {
+                            Console.WriteLine(2);
+                        }
+                        else if (diagP == 1 * mat.GetLength(1) && diagS == 1 * mat.GetLength(1))
+                        {
+                            Console.WriteLine(4);
+                        }
+                        else
+                        {
+                            Console.WriteLine(3);
+                        }
+            }
+        } while (n != 0 && m != 0 );
+
+
+    }
+}
---
+++ libertadores/libertadores/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //Variavel para os times
+    int t1, t2; 
+    //Saldo de gols
+    int s1= 0, s2= 0, m1, v1, m2, v2;
+    //Verificar os números de partidas
+    int n = int.Parse(Console.ReadLine());
+    //for pra pegar o número de partidas
+    for(int i = 0; i < n; i++){
+      //Zerar as váriaveis
+      s1 = 0;
+      s2 = 0;
+      t1 = 0;
+      t2 = 0;
+      //for para preencher o vetor
+      string [] placar1 = Console.ReadLine().Split('x');
+      string [] placar2 = Console.ReadLine().Split('x');
+      m1 = int.Parse(placar1[0]);
+      v1 = int.Parse(placar1[1]);
+      m2 = int.Parse(placar2[0]);
+      v2 = int.Parse(placar2[1]);
+      //pontos por vitória e empate
+        if(m1 > v1){
+          s1 += 3;
+        }
+      else if(v1 > m1){
+          s2 += 3;
+        }
+      else if(m1 == v1){
+          s1 += 1;
+          s2 += 1;
+        }
+      if(m2 > v2){
+          s2 += 3;
+        }
+      else if(m2 < v2){
+          s1 += 3;
+        }
+      else{
+          s1 += 1;
+          s2 += 1;
+        }
+      //saldo de goals
+          t1 = m1 + v2;
+          t2 = v1 + m2;
+          t1 = t1-t2;
+          t2 = (t2-t1) - t2;
+          if(s2>s1){Console.WriteLine("Time 2");}
+          else if(s1>s2){Console.WriteLine("Time 1");}
+          else if(t1>t2){Console.WriteLine("Time 1");}
+          else if(t2>t1){ Console.WriteLine("Time 2");}
+      //gols na casa do adversário
+      else if(v1>v2){Console.WriteLine("Time 2");}
+      else if(v1<v2){Console.WriteLine("Time 1");}
+      // se nenhuma penalti
+      else{Console.WriteLine("Penaltis");}
+    }
+  }
+}
