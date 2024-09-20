8ccc38eaeb0b986587198d6436dddf5438af5607
Henrique de Souza <rike1se@gmail.com>
2024-09-20 13:02:35 -0300

    atualização codigo medalhas

--- quadro de medalhas/Program.cs
+++ quadro de medalhas/Program.cs
@@ -8,7 +8,7 @@ class Program
         int n = 0;
         //criação de um vetor de objeto
         n = int.Parse(Console.ReadLine());
-        Paises [] paises = new Paises[n];
+        Paises [] paises = new Paises[n+1];
         //Estrutura de repetição
         for (int i = 0; i < n; i++)
         {
@@ -16,7 +16,19 @@ class Program
             paises[i] = new Paises(valores[0],int.Parse(valores[1]),int.Parse(valores[2]),int.Parse(valores[3]));
            
         }
-
+        //condições de ordenação
+        for (int i = 0; i < n ; i++)
+        {
+            int aux = paises[i].MedalhasOuro();
+            paises[n+1] = paises[i];
+            int j = i -1;
+            while (j >= 0 && aux < 0)
+            {
+                paises[j + 1] = paises[j];
+                j--;
+            }
+            paises[j + 1] = paises[n+1];
+        }
         //saída dos dados
         foreach (var p in paises)
         {
